046f089d69ec572c61d1a4904cb7e7828ac39a00
Azeem Bukhari <azembukhariprivate@gmail.com>
2020-08-24 21:19:41 +0400

    Mutli Tab Interface

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ Updated on Sunday August 23 20:52:00 2020
 @author: azeembukhariprivate
 """
 import sqlite3
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
 from tkinter import Frame,Tk,Label,Entry,Scrollbar,Listbox,Button,END,VERTICAL
 
 def create_connection(db_file):
@@ -26,12 +29,11 @@ class phonebook(Frame):
         self.load_gui()
         self.load_list()
         
-
     def onClear(self):
     #clears the textboxes
-        self.txt_fname.delete(0,END)
-        self.txt_lname.delete(0,END)
-        self.txt_phone.delete(0,END)
+        self.txt_user.delete(0,END)
+        self.txt_empcode.delete(0,END)
+        self.txt_ipaddress.delete(0,END)
         self.txt_email.delete(0,END)
     def load_list(self):
         print("Load List")
@@ -41,12 +43,12 @@ class phonebook(Frame):
         #selection_id = id_list.curselection()[0]
         #name = id_list.get(selection_id)
         #data = self.my_db.get_record(name)
-        self.txt_fname.delete(0, END)
-        self.txt_fname.insert(0,data[0])
-        self.txt_lname.delete(0, END)
-        self.txt_lname.insert(0,data[1])
-        self.txt_phone.delete(0, END)
-        self.txt_phone.insert(0,data[2])
+        self.txt_user.delete(0, END)
+        self.txt_user.insert(0,data[0])
+        self.txt_empcode.delete(0, END)
+        self.txt_empcode.insert(0,data[1])
+        self.txt_ipaddress.delete(0, END)
+        self.txt_ipaddress.insert(0,data[2])
         self.txt_email.delete(0, END)
         self.txt_email.insert(0,data[3])
     def on_delete(self):
@@ -57,70 +59,147 @@ class phonebook(Frame):
         """
         self.conn.close()
         self.master.destroy()
+        window.destroy()
         
     def connect_db(self):
-        self.conn = create_connection("recipebook.db")
+        self.conn = create_connection("iptable.db")
 
     def onClickFind(self):
         print ("Find")
         cur = self.conn.cursor()
-        cur.execute("SELECT * FROM issuingTable WHERE name LIKE ?", ('%'+ self.txt_fname.get() + '%',))
+        cur.execute("SELECT * FROM ipTable WHERE user LIKE ?", ('%'+ self.txt_user.get() + '%',))
         rows = cur.fetchall()
-        self.txt_phone.delete(0, END)
-        self.txt_fname.delete(0, END)
-        self.txt_fname.insert(0,rows[0][1])
-        self.txt_phone.insert(0,rows[0][10])
+        self.current_name = rows[0][2]
+        self.txt_empcode.delete(0, END)
+        self.txt_ipaddress.delete(0, END)
+        self.txt_email.delete(0, END)
+        self.txt_user.delete(0, END)
+
+        self.txt_user.insert(0,rows[0][2])
+        self.txt_ipaddress.insert(0,rows[0][1])
+        self.txt_empcode.insert(0,rows[0][0])
+        self.txt_email.insert(0,rows[0][3])
 
     def onClickAdd(self):
         print ("Add")
-        sql = ''' INSERT INTO issuingTable (name,whatsapp)
-                  VALUES(?,?) '''
-        data = (self.txt_fname.get(),self.txt_phone.get())
+        sql = ''' INSERT INTO ipTable (empcode,user,address,mail)
+                  VALUES(?,?,?,?) '''
+        data = (self.txt_empcode.get(),self.txt_user.get(),self.txt_ipaddress.get(),self.txt_email.get())
         print ("data : ", data)
         cur = self.conn.cursor()
         cur.execute(sql,data)
         self.conn.commit()
+        self.txt_empcode.delete(0, END)
+        self.txt_ipaddress.delete(0, END)
+        self.txt_email.delete(0, END)
+        self.txt_user.delete(0, END)
+        messagebox.showinfo("Info","User Added") 
+
+
+    def onClickUpdate(self):
+        print ("Add")
+        sql = ''' UPDATE ipTable SET empcode = ? , address = ?, user = ?, mail = ? WHERE user = ? '''
+
+        data = (self.txt_empcode.get(),self.txt_ipaddress.get(),self.txt_user.get(),self.txt_email.get(),self.current_name)
+        print ("data : ", data)
+        self.current_name = self.txt_user.get()
+        cur = self.conn.cursor()
+        cur.execute(sql,data)
+        self.conn.commit()
+        messagebox.showinfo("Info","User Updated") 
     
     def load_gui(self):
     #setting up gui labels
-        self.lbl_fname = Label(self.master, text = 'First Name: ')
+        self.lbl_fname = Label(self.master, text = 'Full Name ')
         self.lbl_fname.grid(row = 0, column = 0, padx = (27,0), pady = (10,0), sticky = 'nw')
-        self.lbl_lname = Label(self.master, text = 'Last Name: ')
+        self.lbl_lname = Label(self.master, text = 'EMP CODE ')
         self.lbl_lname.grid(row = 2, column = 0, padx = (27,0), pady = (10,0), sticky = 'nw')
-        self.lbl_phone = Label(self.master, text = 'Phone: ')
+        self.lbl_phone = Label(self.master, text = 'IP Address ')
         self.lbl_phone.grid(row = 4, column = 0, padx = (27,0), pady = (10,0), sticky = 'nw')
         self.lbl_email = Label(self.master, text = 'Email: ')
         self.lbl_email.grid(row = 6, column = 0, padx = (27,0), pady = (10,0), sticky = 'nw')
-        self.lbl_user = Label(self.master, text = 'User: ')
-        self.lbl_user.grid(row = 0, column = 2, padx = (0,0), pady = (10,0), sticky = 'nw')
+        #self.lbl_user = Label(self.master, text = 'User: ')
+        #self.lbl_user.grid(row = 0, column = 2, padx = (0,0), pady = (10,0), sticky = 'nw')
         #setting up gui entry fields
-        self.txt_fname = Entry(self.master, text = '')
-        self.txt_fname.grid(row = 1, column = 0, columnspan = 2, padx = (30,40), pady = (0,0), sticky = 'new')
-        self.txt_lname = Entry(self.master, text = '')
-        self.txt_lname.grid(row = 3, column = 0, columnspan = 2, padx = (30,40), pady = (0,0), sticky = 'new')
-        self.txt_phone = Entry(self.master, text = '')
-        self.txt_phone.grid(row = 5, column = 0, columnspan = 2, padx = (30,40), pady = (0,0), sticky = 'new')
+        self.txt_user = Entry(self.master, text = '')
+        self.txt_user.grid(row = 1, column = 0, columnspan = 2, padx = (30,40), pady = (0,0), sticky = 'new')
+        self.txt_empcode = Entry(self.master, text = '')
+        self.txt_empcode.grid(row = 3, column = 0, columnspan = 2, padx = (30,40), pady = (0,0), sticky = 'new')
+        self.txt_ipaddress = Entry(self.master, text = '')
+        self.txt_ipaddress.grid(row = 5, column = 0, columnspan = 2, padx = (30,40), pady = (0,0), sticky = 'new')
         self.txt_email = Entry(self.master, text = '')
         self.txt_email.grid(row = 7, column = 0, columnspan = 2, padx = (30,40), pady = (0,0), sticky = 'new')
         #setting up listbox and the scrollbar
-        self.scrollbar1 = Scrollbar(self.master, orient = VERTICAL)
-        self.lstList1 = Listbox(self.master, exportselection = 0, yscrollcommand = self.scrollbar1.set)
-        self.lstList1.bind('<<ListboxSelect>>', self.on_select)
-        self.scrollbar1.config(command = self.lstList1.yview)
-        self.scrollbar1.grid(row = 1, column = 5, rowspan = 7, sticky = 'nes')
-        self.lstList1.grid(row = 1, column = 2, rowspan = 7, columnspan = 3, sticky = 'nsew')
+        #self.scrollbar1 = Scrollbar(self.master, orient = VERTICAL)
+        #self.lstList1 = Listbox(self.master, exportselection = 0, yscrollcommand = self.scrollbar1.set)
+        #self.lstList1.bind('<<ListboxSelect>>', self.on_select)
+        #self.scrollbar1.config(command = self.lstList1.yview)
+        #self.scrollbar1.grid(row = 1, column = 5, rowspan = 7, sticky = 'nes')
+        #self.lstList1.grid(row = 1, column = 2, rowspan = 7, columnspan = 3, sticky = 'nsew')
         #setting up buttons
         self.btn_add = Button(self.master, width = 12, height = 2, text = 'Add', command = lambda: self.onClickAdd())
         self.btn_add.grid(row = 8, column = 0, padx = (25,0), pady = (45,10), sticky = 'w')
         self.btn_add = Button(self.master, width = 12, height = 2, text = 'Find', command = lambda: self.onClickFind())
         self.btn_add.grid(row = 8, column = 1, padx = (25,0), pady = (45,10), sticky = 'w')
-        self.btn_update = Button(self.master, width = 12, height = 2, text = 'Update', command = lambda: phonebook_func.onUpdate(self))
+        self.btn_update = Button(self.master, width = 12, height = 2, text = 'Update', command = lambda: self.onClickUpdate())
         self.btn_update.grid(row = 8, column = 2, padx = (15,0), pady = (45,10), sticky = 'w')
-        self.btn_delete = Button(self.master, width = 12, height = 2, text = 'Delete', command = lambda: phonebook_func.onDelete(self))
-        self.btn_delete.grid(row = 8, column = 3, padx = (15,0), pady = (45,10), sticky = 'w')
+        #self.btn_delete = Button(self.master, width = 12, height = 2, text = 'Delete', command = lambda: phonebook_func.onDelete(self))
+        #self.btn_delete.grid(row = 8, column = 3, padx = (15,0), pady = (45,10), sticky = 'w')
         self.btn_close = Button(self.master, width = 12, height = 2, text = 'Close', command = self.on_close)
-        self.btn_close.grid(row = 8, column = 4, padx = (15,0), pady = (45,10), sticky = 'e')
+        self.btn_close.grid(row = 8, column = 4, padx = (15,15), pady = (45,10), sticky = 'e')
+class App2(ttk.Frame):
+    """ Application to convert feet to meters or vice versa. """
+    def __init__(self, master=None):
+        ttk.Frame.__init__(self, master)
+        self.grid()
+        self.create_widgets()
+
+    def create_widgets(self):
+        """Create the widgets for the GUI"""
+        # 1 textbox (stringvar)
+        self.entry= StringVar()
+        self.textBox1= ttk.Entry(self, textvariable=self.entry).grid(row=0, column=1)
+
+        # 5 labels (3 static, 1 stringvar)
+        self.displayLabel1 = ttk.Label(self, text="feet").grid(row=0, column=2, sticky=W)
+        self.displayLabel2 = ttk.Label(self, text="is equivalent to:").grid(row=1, column=0)
+        self.result= StringVar()
+        self.displayLabel3 = ttk.Label(self, textvariable=self.result).grid(row=1, column=1)
+        self.displayLabel4 = ttk.Label(self, text="meters").grid(row=1, column=2, sticky=W)
+
+        # 2 buttons
+        self.calculateButton = ttk.Button(self, text="Calculate", command=self.convert_feet_to_meters).grid(row=2, column=2, sticky=(S,E))
+        self.quitButton = ttk.Button(self, text="Quit", command=window.destroy).grid(row=2, column=1, sticky=(S,E))
+
+        #exitApplication = tk.Button(root, text='Exit Application', command=root.destroy)
+        #canvas1.create_window(85, 300, window=exitApplication) 
+
+
+    def convert_feet_to_meters(self):
+        """Converts feet to meters, uses string vars and converts them to floats"""
+        self.measurement = float(self.entry.get())
+        self.meters = self.measurement * 0.3048
+        self.result.set(self.meters)
+
+
 if __name__ == "__main__":
-    root = Tk()
-    ph = phonebook(root)
-    root.mainloop()
+    global window
+    window = Tk()
+    window.title("LIFE IT APP")
+
+    notebook = ttk.Notebook(window)
+    frame1 = ttk.Frame(notebook)
+    frame2 = ttk.Frame(notebook)
+    notebook.add(frame1, text="IP Table")
+    notebook.add(frame2, text="Tab2")
+    notebook.grid()
+
+    #Create tab frames
+    app1 = phonebook(master=frame1)
+    app1.grid()
+    app2 = App2(master=frame2)
+    app2.grid()
+
+
+    #ph = phonebook(window)
+    window.mainloop()
